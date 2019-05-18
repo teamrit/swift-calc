c87e8d9ce723650db66a14f5bfa8be6143e27898
Amrit <amritpalsingha4@gmail.com>
2019-05-18 06:36:14 -0400

    add logic for negation, clear and + operator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var Display: UILabel!
 
     let ADDITION = "+";
-    let SUBTRACTION = "+";
-    let MULTIPLICATION = "+";
-    let DIVISION = "+";
+    let SUBTRACTION = "-";
+    let MULTIPLICATION = "*";
+    let DIVISION = "=";
+    
+    let FIRST_OPERATOR = "FIRST_OPERATOR";
     
     let operatorApplied = false;
-
+    
+    var currentOperator = "";
     
     let operators = ["+" , "-" , "*", "/"];
     
@@ -37,15 +40,9 @@ class ViewController: UIViewController {
         Display.translatesAutoresizingMaskIntoConstraints = false
         Display.addConstraint(NSLayoutConstraint(item: Display, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: Display.frame.size.width))
         
-        
-        for button in self.AllButtons {
-            button.layer.cornerRadius = 5
-            button.layer.borderWidth = 1
-            button.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-        }
+        _setButtonsProgrammaticDesign();
         
     }
-
 
     @IBAction func ButtonPressed(_ sender: UIButton) {
         // Declarations
@@ -70,38 +67,94 @@ class ViewController: UIViewController {
         // d -> Checks if "C" is pressed
         let isClearPressed = sender.currentTitle == "C";
         
+        // e -> Negator is pressed
+        let isNegatorPressed = sender.currentTitle == "+/-";
+        
+        // f -> When a decimal is pressed.
+        let isDecimalPressed = sender.currentTitle == ".";
+        
         switch true {
-        case (isButtonPressedNumber):
-            print("Something" , pressedButtonText);
-            let newText = Display.text! == "0" ? "" : Display.text!;
-            self.Display.text = "\(newText)\(pressedButtonText)";
+            case (isButtonPressedNumber):
+                print("Something" , pressedButtonText);
+                let newText = Display.text! == "0" ? "" : Display.text!;
+                self.Display.text = "\(newText)\(pressedButtonText)";
             
-        case (isDeleteButton):
-            let digits = currentText;
-            if (currentText.count != 0 ) {
-                let index1 =
-                    currentText.count == 1 ?
-                        nil : digits.index(digits.endIndex, offsetBy: -2)
-                self.Display.text = currentText.count == 1 ? String(0) : String(digits[...index1!]);
-            }
-        case (isClearPressed) :
-            // FUTURE: History needs to be cleared at this point.
-            print("Clear Pressed")
-            self.Display.text = "0";
-            
-        case (isOperator) :
-            switch (pressedButtonText) {
-                case ("+") :
-                    print("+")
-                default :
-                    print("Default")
+            case (isDeleteButton):
+                let digits = currentText;
+                if (currentText.count != 0 ) {
+                    let index1 =
+                        currentText.count == 1 ?
+                            nil : digits.index(digits.endIndex, offsetBy: -2)
+                    self.Display.text = currentText.count == 1 ? String(0) : String(digits[...index1!]);
+                }
+            case (isNegatorPressed) :
+                print("Negator pressed")
                 
+                let index = currentText.index(currentText.startIndex, offsetBy: 1)
+                print(currentText[index...])
+                
+                self.Display.text = currentText.contains("-") ?
+                    "\(currentText[index...])" : "-\(currentText)";
+                let defaults = UserDefaults.standard
+                if let stringOne = defaults.string(forKey: FIRST_OPERATOR) { print(stringOne)
+
             }
             
-        default:
-            print("Default");
+            case (isClearPressed) :
+                // FUTURE: History needs to be cleared at this point.
+                print("Clear Pressed")
+                self.Display.text = "0";
+            case (isDecimalPressed) :
+                if (!currentText.contains(".")) {
+                    self.Display.text = "\(currentText).";
+                }
+            case (isOperator) :
+                _setStringInStore(key: FIRST_OPERATOR, value: currentText);
+                switch (pressedButtonText) {
+                    case ("+") :
+                        _setStringInStore(key: FIRST_OPERATOR, value: currentText)
+                        currentOperator = "+";
+                        self.Display.text = "0";
+                        
+                        
+                        print("+")
+                    default :
+                        print("Default")
+                    
+                }
+            
+            default:
+                print("Default");
         }
 
     }
+    
+    func _setStringInStore(key : String, value: String) {
+        // Setting
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key )
+    }
+    
+    func _setButtonsProgrammaticDesign() {
+        for button in self.AllButtons {
+            button.layer.cornerRadius = 22
+            button.layer.borderWidth = 1
+            button.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    func _getStringFromStore(key : String) -> String {
+        let defaults = UserDefaults.standard
+        if let stringOne = defaults.string(forKey: key) {
+            return (stringOne)
+        } else {
+            return "Error"
+        }
+    }
+    
+//    func _doArithmetic(firstOperator: String, sth: String, secondOperator: String) -> Double {
+//        sth == ADDITION ? return (Double(firstOperator) + Double(secondOperator)); : return Double("0.0");
+//    }
+    
 }
 
